da44a902752f54dcba837d11f0453049e40f0fda
Andrey Kovalev <avkovaleff@gmail.com>
2026-03-08 19:34:09 -0700

    Remove legacy reddit backfill tool

--- scripts/backfill_reddit_links.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Wrapper for running packaged Reddit backfill utility."""
-
-from bcn.ops.backfill_reddit_links import main
-
-
-if __name__ == "__main__":
-    raise SystemExit(main())
